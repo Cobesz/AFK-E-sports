cfc5c6a6d649f9036130fc3d7a4208d6a4211353
Cobesz <casvantwout@gmail.com>
2015-11-11 16:22:04 +0100

    All styling should be fixed now

--- js/stickynav.js
+++
@@ -1,6 +0,0 @@
-console.log("Started: stickynav.js");
-
-$(function(){
-            $('#stickyheader').css({position: 'fixed', top: '50%'});
-            $('#stickyheader').css({position: 'fixed', right: '0px'});
-  });
